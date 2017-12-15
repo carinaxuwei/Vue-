48ac8f5bfa359b90cf35e6f597191f742618a8e3
LT <296082202@qq.com>
2017-12-15 15:55:13 +0800

    index-firstDay

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,29 +1,15 @@
 <template>
   <div id="app">
-    {{abc}}
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      abc: 1
-    }
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ my-project/src/page/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <header class="header">
+      <div class="back"><i class="icon">返回</i></div>
+      <div class="search"></div>
+      <div class="city">城市</div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .header {
+  	display: flex;
+  	background: #05bad5;
+  	color: #fff;
+  }
+  .back{
+  	width: .64rem;
+  	line-height: .86rem;
+  }
+  .search {
+    flex: 1;
+    margin: .14rem .18rem;
+    background: #fff;
+    border-radius: .1rem;
+  }
+  .city {
+    width: 1.14rem;
+    line-height: .86rem;
+    text-align: center;
+  }
+</style>
