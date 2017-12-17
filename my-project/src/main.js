95d9cd03630050a3ee53935bcd4647d28a783d7d
LT <296082202@qq.com>
2017-12-17 19:10:25 +0800

    剩余footer部分

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'swiper/dist/css/swiper.css'
 import './assets/style/style.css'
 import './assets/iconfont/iconfont.css'
 import VueSwiper from 'vue-awesome-swiper'
+import VueResource from 'vue-resource'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 Vue.use(VueSwiper)
+Vue.use(VueResource)
 
 new Vue({
   el: '#app',
